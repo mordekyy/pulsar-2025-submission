7a881d27de56ac95742cb90fd2b2056f8f63b59c
lazar <dasidasi86@gmail.com>
2025-10-14 08:29:40 +0300

    config module

---
+++ pathplanner/config.py
@@ -0,0 +1,4 @@
+from math import radians
+IMAGE_SIZE = 100
+MAX_SLOPE_DEG = 30
+MAX_SLOPE_RAD = radians(MAX_SLOPE_DEG)
--- pathplanner/main.py
+++ pathplanner/main.py
@@ -1,7 +1,9 @@
 from image.gen import noisy_square
 from image.process import get_red, normalize
+from config import IMAGE_SIZE, MAX_SLOPE_DEG, MAX_SLOPE_RAD
 
-img = noisy_square(100)
+print(f"Generating image with {IMAGE_SIZE}")
+img = noisy_square(IMAGE_SIZE)
 img.save("output/noisy.png")
 
 red_img = get_red(img)
@@ -10,3 +12,4 @@ red_img.save("output/red.png")
 n = normalize(red_img)
 
 print(n)
+print(f"Max slope is {MAX_SLOPE_DEG}deg")
