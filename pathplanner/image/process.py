9009c5e37b17c787537706058b747aa8087ada45
lazar <dasidasi86@gmail.com>
2025-10-14 08:26:18 +0300

    normalized image

--- pathplanner/image/process.py
+++ pathplanner/image/process.py
@@ -1,6 +1,12 @@
 from PIL import Image
+import numpy as np
 
 
 def get_red(img: Image.Image) -> Image.Image:
     r, g, b = img.split()
     return r
+
+
+def normalize(img: Image.Image) -> np.ndarray:
+    arr = np.asarray(img, dtype=np.float32)
+    return arr / 255.0
--- pathplanner/main.py
+++ pathplanner/main.py
@@ -1,8 +1,12 @@
 from image.gen import noisy_square
-from image.process import get_red
+from image.process import get_red, normalize
 
 img = noisy_square(100)
 img.save("noisy.png")
 
 red_img = get_red(img)
 red_img.save("red.png")
+
+n = normalize(red_img)
+
+print(n)
