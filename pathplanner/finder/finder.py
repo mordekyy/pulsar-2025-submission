8d00a453b859c134e725baedca6c057551e3f3c5
lazar <dasidasi86@gmail.com>
2025-10-14 16:17:57 +0300

    basic a_star boilerplate

--- pathplanner/finder/finder.py
+++ pathplanner/finder/finder.py
@@ -1,4 +1,5 @@
 from math import hypot
+from heapq import heappop
 
 
 def is_valid(row, col, ROW, COL):
@@ -27,3 +28,25 @@ def is_destination(row, col, dest):
 
 def remaining_path(row, col, dest):
     return hypot(row - dest[0], col - dest[1])
+
+
+def a_star(grid, start, end):
+    g = {start: 0.0}
+    parent = {start: start}  # TODO: handle parents
+    f0 = g[start] + remaining_path(*start, end)
+
+    openq = [(f0, start)]  # todo: add children here
+
+    closed = set()
+
+    while openq:
+        _, cur = heappop(openq)
+        if cur in closed:
+            continue
+        if cur == end:
+            if start == end:
+                return [start]
+            return None  # am terminat aici
+        closed.add(cur)
+
+    return None
