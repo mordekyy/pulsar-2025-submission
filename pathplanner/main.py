b01e6dcf8fe3c22fa285c2e90a73f4f4f33c8ecb
lazar <dasidasi86@gmail.com>
2025-10-14 08:26:43 +0300

    output folder

--- pathplanner/main.py
+++ pathplanner/main.py
@@ -2,10 +2,10 @@ from image.gen import noisy_square
 from image.process import get_red, normalize
 
 img = noisy_square(100)
-img.save("noisy.png")
+img.save("output/noisy.png")
 
 red_img = get_red(img)
-red_img.save("red.png")
+red_img.save("output/red.png")
 
 n = normalize(red_img)
 
