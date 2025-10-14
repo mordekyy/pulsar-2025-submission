a4b08c1e9668a2cf16771c7373ea9e8403e78b4a
lazar <dasidasi86@gmail.com>
2025-10-14 16:24:50 +0300

    a start populate the priority queue

--- pathplanner/finder/finder.py
+++ pathplanner/finder/finder.py
@@ -1,5 +1,7 @@
 from math import hypot
-from heapq import heappop
+from heapq import heappop, heappush
+from config import MovementMode
+from convert import directions_from_movement_mode
 
 
 def is_valid(row, col, ROW, COL):
@@ -31,22 +33,42 @@ def remaining_path(row, col, dest):
 
 
 def a_star(grid, start, end):
+    start = tuple(start)
+    end = tuple(end)
+
+    dirs = directions_from_movement_mode(MovementMode.FOUR_DIRECTIONS)
+
+    ROW, COL = len(grid), len(grid[0])
+
     g = {start: 0.0}
     parent = {start: start}  # TODO: handle parents
     f0 = g[start] + remaining_path(*start, end)
 
     openq = [(f0, start)]  # todo: add children here
 
-    closed = set()
+    visited = set()
 
     while openq:
         _, cur = heappop(openq)
-        if cur in closed:
+        if cur in visited:
             continue
         if cur == end:
             if start == end:
                 return [start]
-            return None  # am terminat aici
-        closed.add(cur)
+            return None
+
+        visited.add(cur)
+
+        r, c = cur
+        for dr, dc in dirs:
+            nr, nc = r + dr, c + dc
+            if not is_valid(nr, nc) or not is_unblocked(nr, nc) or (nr, nc) in visited:
+                continue
+            ng = g[cur] + 1.0
+            if ng < g.get((nr, nc), float("inf")):
+                g[(nr, nc)] = ng
+                parent[(nr, nc)] = cur
+                f = ng + remaining_path(nr, nc, end)
+                heappush(openq, (f, (nr, nc)))
 
     return None
